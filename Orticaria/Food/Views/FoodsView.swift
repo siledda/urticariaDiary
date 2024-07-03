0861e98cce87ad4616e68a41b2e4a1e53b4f8544
Silvia Ledda <silvia.ledda@wuerth.it>
2024-07-03 19:35:12 +0200

    modificata stringa

--- Orticaria/Food/Views/FoodsView.swift
+++ Orticaria/Food/Views/FoodsView.swift
@@ -32,7 +32,7 @@ struct FoodsView: View {
                 }
             }
             
-            .navigationBarTitle("Lista Espandibile")
+            .navigationBarTitle("Lista")
             .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt:  "Search food items")
 
             .listStyle(.grouped)
